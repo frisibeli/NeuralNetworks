0cf1a41b4b00bc52bd7884bb56c33e5ceee2598d
Polina <polina.krusteva.95@gmail.com>
2016-05-18 22:59:24 +0300

    Add JAMA

--- src/NeuralNetwork.java
+++ src/NeuralNetwork.java
@@ -1,17 +1,24 @@
-			
+import Jama.Matrix;
+
 public class NeuralNetwork {
+
 	private int inputLayerSize;
 	private int outputLayerSize;
 	private int hiddenLayerSize;
-	
-	
-	private double sigmoid(double z){
-		return 1/(1+Math.exp(-z));
-	}
-	
-	public NeuralNetwork(){
+	private Matrix input;
+	private Matrix weights1;
+	private Matrix weights2;
+
+	public NeuralNetwork() {
 		this.hiddenLayerSize = 3;
 		this.outputLayerSize = 1;
 		this.inputLayerSize = 2;
+		this.weights1 = Matrix.random(inputLayerSize, hiddenLayerSize);
+		this.weights2 = Matrix.random(hiddenLayerSize, outputLayerSize);
 	}
+
+	private double sigmoid(double z) {
+		return 1 / (1 + Math.exp(-z));
+	}
+
 }
